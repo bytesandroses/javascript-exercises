29d48422dd6fc4ddd27dc404354f375cc72de64b
Isaac <59330021+bytesandroses@users.noreply.github.com>
2024-03-14 21:55:43 +0200

    Restored repeatString() function to return empty string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,12 +1,5 @@
 const repeatString = function(string, num) {
-  if (num < 0) {
-    return "ERROR";
-  }
-  let stringRepeated = "";
-  for (let i = 1; i <= num; i++) {
-    stringRepeated += string;
-  } 
-  return stringRepeated;
+  
 };
 
 // Do not edit below this line
