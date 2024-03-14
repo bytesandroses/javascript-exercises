98a86f6d5d2684a7a229f2797049820285a8f7de
Isaac <59330021+bytesandroses@users.noreply.github.com>
2024-03-14 22:08:54 +0200

    Restore reverseString() to return an empty value

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,12 +1,5 @@
-const reverseString = function(string) {
-  let stringLength = string.length;
-  let reversedString = "";
-
-  for (let i = stringLength - 1; i >= 0; i--) {
-    reversedString += string[i];
-  }
-
-  return reversedString;
+const reverseString = function() {
+  
 };
 
 // Do not edit below this line
