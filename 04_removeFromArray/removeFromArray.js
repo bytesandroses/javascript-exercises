82ae70cb4f444f3289c2d1e40596a14d93283a3d
Isaac <59330021+bytesandroses@users.noreply.github.com>
2024-03-16 03:50:30 +0200

    Improve readability and succinctness of removeFromArray() function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,32 +1,13 @@
-const removeFromArray = function() {
-  let argsLength = arguments.length;
-  let initialArray = arguments[0];
-  let initialArrayLength = initialArray.length;
-  let finalArray = [];
-
-  for (let i = 0; i < initialArrayLength; i++) {
-    finalArray.push(initialArray[i]);
-    for (let j = 0; j < argsLength - 1; j++) {
-      if (initialArray[i] === arguments[j + 1]) {
-        finalArray.pop();
-      }
+const removeFromArray = function(initialArray, ...itemsToRemove) {
+  const newArray = [];
+  for (let index in initialArray) {
+    let valueAtIndexI = initialArray[index];
+    if (!itemsToRemove.includes(valueAtIndexI)) {
+      newArray.push(valueAtIndexI);
     }
   }
-
-  return finalArray;
-};
-
-/* SOLUTION 2
-const removeFromArray = function (array, ...args) {
-  const newArray = [];
-  array.forEach((item) => {
-    if (!args.includes(item)) {
-      newArray.push(item);
-    }
-  });
   return newArray;
 };
-*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
