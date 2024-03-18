a0de7f6a3b45b516bbdacd9b605246a05654af60
Isaac <59330021+bytesandroses@users.noreply.github.com>
2024-03-18 02:04:40 +0200

    Completed sumAll() function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
 const sumAll = function() {
-
+  let minNum = arguments[0];
+  let maxNum = arguments[1];
+  let sumNumbers = 0;
+  for (let i = minNum; i < maxNum + 1; i++) {
+    sumNumbers += i;
+  }
+  return sumNumbers;
 };
 
 // Do not edit below this line
