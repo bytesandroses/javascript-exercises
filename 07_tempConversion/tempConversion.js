e8d35a8c211a8291cfea8c2d87cd1344561f026b
Isaac <59330021+bytesandroses@users.noreply.github.com>
2024-03-19 17:45:21 +0200

    Edited functions to round results to 1 decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureFahrenheit) {
+  // °C = (°F - 32) × 5/9
+  let temperatureCelsius = (Math.round((((temperatureFahrenheit - 32) * 5) / 9) * 10)) / 10;
+  return temperatureCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureCelsius) {
+  // °F = (9/5 × °C) + 32
+  let temperatureFahrenheit = (Math.round((((9/5) * temperatureCelsius) + 32) * 10)) / 10;
+  temperatureFahrenheit = (temperatureFahrenheit * 10) / 10;
+  return temperatureFahrenheit;
 };
 
 // Do not edit below this line
